f4081efc00bbd628fb703a0876e9d8674a3e7947
Del <34488721+Del-Cisco@users.noreply.github.com>
2017-12-12 20:01:47 +0000

    Update xerxes.c
    xerxes with full power

--- xerxes.c
+++ xerxes.c
@@ -49,8 +49,8 @@ void broke(int s) {
 	// do nothing
 }
 
-#define CONNECTIONS 8
-#define THREADS 48
+#define CONNECTIONS 80
+#define THREADS 100
 
 void attack(char *host, char *port, int id) {
 	int sockets[CONNECTIONS];
